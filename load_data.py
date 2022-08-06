8a496bc102a895057ceef054794bdb22a593842a
lmunozbohollo <lucia.munozbohollo@gmail.com>
2022-08-06 22:50:43 +0100

    Observe image data

--- load_data.py
+++ load_data.py
@@ -4,19 +4,34 @@ Uploading neuroimages for processing
 @author: Luciamb
 """
 
-import os
-
+import nibabel as nib
+import matplotlib.pyplot as plt
+import PyQt5
 
 # select folder where the data is stored
-path = "FLAIR/"
+scan_number = 28
+path = "FLAIR/BraTS2021_{}_flair.nii.gz".format(str(scan_number).zfill(5))
 
-image = os.listdir(path)
 
-# empty list to add paths to all images
-all_images_paths = []
+# upload the desired scan from the folder
+scan = nib.load(path)
 
-for i in image:
-    path=os.path.join(path,i)
-    all_images_paths.append(path)
-    
-print(all_images_paths)
+# to show scan as matrices
+scan_data = scan.get_fdata()
+#print(scan_data)
+
+
+header=scan.header
+#print(header)
+
+
+# to observe how many slices were taken in each direction
+shape=scan.shape
+print(shape)
+
+
+z_slice = 60
+plt.imshow(scan_data[:,:,z_slice]) # select the desired plane and slice on that plane
+plt.title('Scan from patient {}, z={}'.format(str(scan_number),z_slice))
+plt.show()
+# conseguir cambiar x e y tambien
