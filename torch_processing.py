ee924b5a30ba5ca15953b9db492b4e9cc4492065
lmunozbohollo <lucia.munozbohollo@gmail.com>
2022-08-09 18:19:28 +0100

    Flip transform added

--- torch_processing.py
+++ torch_processing.py
@@ -24,8 +24,8 @@ scan_in_tensor = image_to_tensor(scan_data)
 """
  to rotate the image by the desired number of degrees
 """
-random_rotation = transforms.RandomRotation(45)
-rotation = transforms.Compose([random_rotation])
+desired_rotation = transforms.RandomRotation(45)
+rotation = transforms.Compose([desired_rotation])
 
 scan_rotated = rotation(scan_in_tensor)
 #plt.imshow(scan_rotated[:,:,50])
@@ -44,9 +44,30 @@ scan_resized = resize(scan_in_tensor)
 """
  crop the image
 """
-random_cropping = transforms.Compose([transforms.RandomResizedCrop(120)])
-scan_cropped = random_cropping(scan_in_tensor)
-plt.imshow(scan_cropped[:,:,50])
-plt.title('Scan cropped')
-print(scan_cropped.shape)
+cropping = transforms.Compose([transforms.RandomResizedCrop(120)])
+scan_cropped = cropping(scan_in_tensor)
+#plt.imshow(scan_cropped[:,:,50])
+#plt.title('Scan cropped')
+#print(scan_cropped.shape)
+
+
+"""
+ horizontal mirroring (flip)
+"""
+horizontal_flip = transforms.Compose([transforms.RandomHorizontalFlip()])
+scan_flipped = horizontal_flip(scan_in_tensor)
+#plt.imshow(scan_flipped[:,:,50])
+#plt.title('Scan flipped')
+
+
+"""
+ affine transform
+ select the unmoving centre where desired and transform the image about that centre
+"""
+affine_transf = transforms.Compose([transforms.RandomAffine(20)])
+scan_affine = affine_transf(scan_in_tensor)
+plt.imshow(scan_affine[:,:,50])
+plt.title('Scan with affine 20 deg')
+
+
 
